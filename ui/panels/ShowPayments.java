afb5cb25c0c3bd43377a9edd849017d425084023
Wadyweb73 <wadymunguambe73@gmail.com>
2024-05-21 19:13:46 +0200

    feat: adiciona outras informações do cliente ao clicar no usuario na lista de pagamentos

--- ui/panels/ShowPayments.java
+++ ui/panels/ShowPayments.java
@@ -1,29 +1,52 @@
 package ui.panels;
 
+import static ui.panels.ServedClients.configureActionButton;
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
 
-public class ShowPayments {
+import database.DBConnection;
+
+public class ShowPayments implements MouseListener, ActionListener {
     public static JScrollPane scrollPane;
     public static DefaultTableModel tableModel;
+    public static JPanel description_panel, date_panel, receiptCode_panel;
     public static JTable table;
-    public static JPanel mainPanel;
-    public static JButton payment_button;
+    public static JPanel mainPanel, additionalInformationPanel;
+    public static JButton goToReceiptButton;
 
     public ShowPayments () {
         tableModel                 = configureTableModel();
         table                      = configureTable(tableModel);
         scrollPane                 = configureScrollPane(table);
+        goToReceiptButton          = configureActionButton();
+        description_panel          = createInfoPanel("Problema:", null);
+        date_panel                 = createInfoPanel("Data de requisição: ", null);
+        receiptCode_panel          = createInfoPanel("Numero de Recibo", null);
+        additionalInformationPanel = configureAdditionalInformationPanel();
         mainPanel                  = configureMainPanel();
+
+        goToReceiptButton.addActionListener(this);
     }
     
    public DefaultTableModel configureTableModel() {
@@ -47,22 +70,44 @@ public class ShowPayments {
 		table.setFont(new Font("consolas", Font.PLAIN, 15));
 		table.setRowHeight(30);
         table.setFocusable(false);
-
-        for (int i = 0; i < table.getRowCount(); i++) {
-            for (int y = 0; y < table.getColumnCount(); y++) {
-                table.isCellEditable(i, y);
-            }
-        }
         
         JTableHeader header = table.getTableHeader();
         header.setBackground(new Color(0x123456));
         header.setForeground(new Color(0xcdcdcd));                    
+
+        table.addMouseListener(this);
         
         return table;
     }
 
     boolean isCellEditable(int row, int column) {
         return false;
+    }
+
+    private JPanel createInfoPanel(String label, String value) {
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+        panel.setBackground(new Color(0x123456));
+        panel.setPreferredSize(new Dimension(800, 50));
+
+        JLabel labelComponent = new JLabel(label);
+        labelComponent.setFont(new Font("Consolas", Font.ROMAN_BASELINE, 14));
+        labelComponent.setForeground(new Color(0xffffff));
+
+        JLabel valueComponent = new JLabel(value);
+        valueComponent.setFont(new Font("Consolas", Font.PLAIN, 14));
+        valueComponent.setForeground(new Color(0xffffff));
+
+        panel.add(labelComponent, BorderLayout.WEST);
+        panel.add(valueComponent, BorderLayout.EAST);
+
+        return panel;
+    }
+
+    public void updateAdditionalInfo(String description, String request_date, String receipt_code) {
+        ((JLabel)description_panel.getComponent(1)).setText(description);
+        ((JLabel)receiptCode_panel.getComponent(1)).setText(receipt_code);
+        ((JLabel)date_panel       .getComponent(1)).setText(request_date);
     }
 
     public JScrollPane configureScrollPane(JTable table) {
@@ -76,6 +121,23 @@ public class ShowPayments {
         return scrollPane;
     }
 
+    public JPanel configureAdditionalInformationPanel() {
+        JPanel panel  = new JPanel();
+
+        panel.setPreferredSize(new Dimension(100, 300));
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        panel.setBackground(new Color(0x123456));
+    
+        panel.add(description_panel);
+        panel.add(date_panel);
+        panel.add(receiptCode_panel);
+        panel.add(Box.createRigidArea(new Dimension(0, 7))); // Espaçamento entre os detalhes e o botão
+        panel.add(goToReceiptButton);
+        panel.add(Box.createRigidArea(new Dimension(0, 18))); // Espaçamento entre os detalhes e o botão
+
+        return panel;
+    }
+
     public JPanel configureMainPanel() {
         JPanel panel = new JPanel();
 
@@ -87,8 +149,51 @@ public class ShowPayments {
 
         return panel;
     }
+
+//===================================================================================
+
+    @Override
+    public void mouseEntered(MouseEvent event) {
+        if (event.getSource() == goToReceiptButton) {
+            goToReceiptButton.setBackground(new Color(60, 179, 113));
+            goToReceiptButton.setForeground(new Color(0x123456));
+        }
+    }
+    public void mouseExited(MouseEvent event) {
+        if (event.getSource() == goToReceiptButton) {
+            goToReceiptButton.setBackground(new Color(0xcdcdcd));
+        }
+    }
+    public void mouseClicked(MouseEvent event) {
+        int line                 = table.getSelectedRow();
+        int column               = 0;
+        String query             = "SELECT * FROM comments INNER JOIN client ON comments.client_id = client.id WHERE client.name = ?";
+
+        try {
+            PreparedStatement ps = DBConnection.getConexao().prepareStatement(query);
+            ps.setString(1, (String) table.getValueAt(line, column));
+            ResultSet res        = ps.executeQuery(); 
+
+            if (res.next()) {
+                updateAdditionalInfo(res.getString("discription"), res.getString("task_date"), "dsgjhhj");
+            }
+            
+            mainPanel.add(additionalInformationPanel, BorderLayout.SOUTH);
+            mainPanel.repaint();
+            mainPanel.revalidate();
+        }
+        catch(SQLException e) {
+            e.getMessage();
+        }
+        
+    }
+    public void mousePressed(MouseEvent arg0) {}
+    public void mouseReleased(MouseEvent arg0) {}   
     
 	public static void addContentFromMySQL(Object[] obj) {
 		tableModel.addRow(obj);
 	}
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {}
 }
